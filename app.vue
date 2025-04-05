6083220195cf9271da3f4343177682569ed383e8
T4kano <edutakajr@gmail.com>
2025-04-05 16:46:00 -0300

    first commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup>
+// import "flag-icon-css/css/flag-icons.min.css"
+
+// useAuthStore().$hydrate({ runHooks: false })
+useHead({
+    title: "RNO Brava",
+    link: [
+        { rel: "icon", type: "image/x-icon", href: "/sapi4/client/favicon.ico" }
+    ]
+})
+</script>
+
+<template>
+<div class="bg-neutral-50 dark:bg-[#0d1321] min-h-screen">
+	<NuxtLoadingIndicator />
+	<UApp>
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</UApp>
+</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+	<h1>Home</h1>
+	<UButton label="Click me" @click="() => alert('Hello!')" />
+</div>
+</template>
