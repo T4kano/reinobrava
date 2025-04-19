fe24d6aab1f85d8e5ea5377b74c22c795c757a4c
T4kano <edutakajr@gmail.com>
2025-04-19 15:35:56 -0300

    Updates

--- assets/BVwd6OMJ.js
+++
@@ -1,1 +0,0 @@
-import"./C63PWevn.js";const t=""+new URL("brand.BvelbFXZ.png",import.meta.url).href;export{t as _};
--- assets/T9_VQEGG.js
+++
@@ -1,1 +0,0 @@
-import{c as e,o as c}from"./C63PWevn.js";const r={__name:"schedule",setup(t){return(a,n)=>(c(),e("h1",null,"Agenda"))}};export{r as default};
